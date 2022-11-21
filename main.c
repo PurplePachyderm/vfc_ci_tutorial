745488f0393fb253238211e2e671082424e28bfb
3773 <delval@olympelogin1.bullx>
2022-11-21 14:50:32 +0100

    Add recursive dotprod

--- main.c
+++ main.c
@@ -4,6 +4,25 @@
 #include <vfc_probes.h>
 #define N 4096
 
+float recursiveDotprod(float * x, float * y, size_t n) {
+	// NOTE This implementation assumes that N can be written as 2^k
+	if((n & (n - 1)) != 0) {
+		return 0;
+	}
+
+	// Base case
+	if(n == 1) {
+		return x[0] * y[0];
+	}
+
+	// Recursive case
+	else {
+		// Split array in 2 and do a recursive call for each half
+		return recursiveDotprod(x, y, n / 2) +
+			recursiveDotprod(&(x[n/2]), &(y[n/2]), n / 2);
+	}
+}
+
 
 float naiveDotprod(float * x, float * y, size_t n) {
 	float res = 0;
@@ -30,6 +49,8 @@ int main(void) {
 	float naiveRes = naiveDotprod(x, y, N);
 	vfc_probe(&probes, "dotprod_test", "naive", naiveRes);
 
+	float recursiveRes = recursiveDotprod(x, y, N);
+                vfc_probe(&probes, "dotprod_test", "recursive", recursiveRes);
 	printf("Naive dotprod = %.7f \n", naiveRes);
 
          vfc_dump_probes(&probes);
